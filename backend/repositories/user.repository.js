c203ff8d5301a9f39ee107fd1aa5b352a6fb8713
dosic <codingdosic@gmail.com>
2025-10-16 02:56:19 +0900

    feat: Enhance UserRepository with recommendation and menu management methods

--- backend/repositories/user.repository.js
+++ backend/repositories/user.repository.js
@@ -20,6 +20,11 @@ class UserRepository {
     return await User.findById(userId);
   }
 
+  // 사용자 이름으로 사용자 찾기
+  async findUserByUsername(username) {
+    return await User.findOne({ username });
+  }
+
   // 사용자 비밀번호 업데이트
   async updateUserPassword(userId, newHashedPassword) {
     return await User.findByIdAndUpdate(userId, { password: newHashedPassword }, { new: true });
@@ -28,6 +33,41 @@ class UserRepository {
   // 사용자 이메일 업데이트
   async updateUserEmail(userId, newEmail) {
     return await User.findByIdAndUpdate(userId, { email: newEmail }, { new: true });
+  }
+
+  // 사용자에게 메뉴 추가
+  async addMenuToUser(userId, menuId) {
+    return await User.findByIdAndUpdate(userId, { $addToSet: { menus: menuId } }, { new: true });
+  }
+
+  // 사용자에게서 메뉴 제거
+  async removeMenuFromUser(userId, menuId) {
+    return await User.findByIdAndUpdate(userId, { $pull: { menus: menuId } }, { new: true });
+  }
+
+  // 사용자 추천 메뉴 추가
+  async addRecommendedMenuToUser(userId, menuId) {
+    return await User.findByIdAndUpdate(userId, { $addToSet: { recommendedMenus: menuId }, $pull: { disrecommendedMenus: menuId } }, { new: true });
+  }
+
+  // 사용자 추천 메뉴 제거
+  async removeRecommendedMenuFromUser(userId, menuId) {
+    return await User.findByIdAndUpdate(userId, { $pull: { recommendedMenus: menuId } }, { new: true });
+  }
+
+  // 사용자 비추천 메뉴 추가
+  async addDisrecommendedMenuToUser(userId, menuId) {
+    return await User.findByIdAndUpdate(userId, { $addToSet: { disrecommendedMenus: menuId }, $pull: { recommendedMenus: menuId } }, { new: true });
+  }
+
+  // 사용자 비추천 메뉴 제거
+  async removeDisrecommendedMenuFromUser(userId, menuId) {
+    return await User.findByIdAndUpdate(userId, { $pull: { disrecommendedMenus: menuId } }, { new: true });
+  }
+
+  // 사용자 추천/비추천 메뉴 상태 가져오기
+  async getUserRecommendations(userId) {
+    return await User.findById(userId).select('recommendedMenus disrecommendedMenus');
   }
 
   // 사용자 삭제
